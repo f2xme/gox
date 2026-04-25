0d0ddfa9b4a8a5550c664c36d259234344adbdd9
Bran <42071814+f2xb@users.noreply.github.com>
2026-04-25 17:15:54 +0800

    feat(captcha/cache): add MustNewCaptcha

--- captcha/adapter/cache/captcha.go
+++ captcha/adapter/cache/captcha.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"log"
+
 	"github.com/f2xme/gox/captcha"
 	"github.com/f2xme/gox/captcha/generator/base64"
 )
@@ -20,4 +22,13 @@ func NewCaptcha(c Backend, opts ...CaptchaOption) (captcha.Service, error) {
 	store := New(c)
 	cfg.captchaOpts = append(cfg.captchaOpts, captcha.WithGenerator(gen))
 	return captcha.New(store, cfg.captchaOpts...)
+}
+
+// MustNewCaptcha 创建使用 cache 存储的 Service 实例，创建失败时退出程序。
+func MustNewCaptcha(c Backend, opts ...CaptchaOption) captcha.Service {
+	svc, err := NewCaptcha(c, opts...)
+	if err != nil {
+		log.Fatalf("captcha/cache: create captcha failed: %v", err)
+	}
+	return svc
 }
